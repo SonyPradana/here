f500d8ce34782d3bf11fa2b7eb910082875c40c4
Angger Pradana <sonypradana@gmail.com>
2022-10-02 13:03:44 +0700

    [Style] Redesigned Array Printer Style (#15)
    * refactor: new array printer
    
    * feat: add maximum line printed
    
    * refactor: rename property and method

--- src/Styles/ArrayStyle.php
+++ src/Styles/ArrayStyle.php
@@ -5,16 +5,57 @@ declare(strict_types=1);
 namespace Here\Styles;
 
 use Here\Abstracts\VarPrinter;
+use Here\Config;
 use System\Console\Style\Style;
 
 class ArrayStyle extends VarPrinter
 {
+    /**
+     * @param Style $style
+     */
+    public function __construct($style)
+    {
+        parent::__construct($style);
+        $this->max_line = (int) Config::get('print.var.max', 5);
+    }
+
     public function render(): Style
     {
-        $var = $this->sanitize($this->var);
+        /** @var array<int|string, mixed> */
+        $var = $this->var;
 
-        return $this->style
-            ->new_lines()
-            ->push($var)->textGreen();
+        $this->style
+            ->push('array:')->textBlue()
+            ->push(count($var))->textBlue()
+            ->push(' [')->textYellow()
+        ;
+
+        foreach ($var as $key => $value) {
+            $this->current_line++;
+            if ($this->current_line > $this->max_line) {
+                $this->style->new_lines()->repeat(' ', $this->tab_size * 2)->push('...')->textDim();
+                break;
+            }
+
+            $this->style->new_lines();
+            $this->style->repeat(' ', $this->tab_size * 2);
+            $this->style->push($key)->textLightGreen();
+            $this->style->push(' => ')->textYellow();
+
+            $style = is_array($value)
+                ? new ArrayStyle($this->style)
+                : new VarStyle($this->style);
+
+            $style->ref($value)
+                ->tabSize($this->tab_size + 1)
+                ->currentLine($this->current_line)
+            ;
+            $this->style = $style->render();
+        }
+
+        $this->style->new_lines()->repeat(' ', ($this->tab_size * 2) - 2);
+        $this->style->push(']')->textYellow();
+
+        return $this->style;
     }
 }
